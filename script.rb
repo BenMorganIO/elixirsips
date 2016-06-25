880b67d946930fffda11d1ebe97789678617babf
Ben A. Morgan <ben@benmorgan.io>
2016-06-24 18:34:21 -0700

    cleanup the install process
    Get told if you already have the file downloaded.
    Download from bottom to top.

--- script.rb
+++ script.rb
@@ -26,19 +26,26 @@ def authenticate(page)
   end.submit
 end
 
+def download_video(agent, link)
+  post = agent.get BASE_URI + link.uri.to_s
+  video_link = post.links.find { |l| l.text.end_with? '.mp4' }
+  video_name = video_link.text.gsub('_', ' ')
+
+  if File.exists? "./videos/#{video_name}"
+    puts "Already Downloaded #{video_name}"
+    return
+  end
+
+  puts "Downloading #{video_name.sub(/.mp4\z/, '')}..."
+  agent.get(BASE_URI + video_link.uri.to_s).save("./videos/#{video_name}")
+end
+
 agent = Mechanize.new
 page = agent.get(BASE_URI + '/subscriber/content')
 
 if content = authenticate(page)
-  content.links.each do |link|
-    next unless link.text =~ /file attachment/i
-    post = agent.get BASE_URI + link.uri.to_s
-    video_link = post.links.find { |l| l.text.end_with? '.mp4' }
-    video_name = video_link.text.gsub('_', ' ')
-    next if File.exists? "./videos/#{video_name}"
-
-    puts "Downloading #{video_name.sub(/.mp4\z/, '')}..."
-    agent.get(BASE_URI + video_link.uri.to_s).save("./videos/#{video_name}")
+  content.links.reverse.each do |link|
+    download_video agent, link if link.text =~ /file attachment/i
   end
 else
   puts 'Unable to authenticate.'
